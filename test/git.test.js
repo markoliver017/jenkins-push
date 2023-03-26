4a873a434edfdd845ccdd17e58d9363cb67e3277
oliver <oliver@example.com>
2023-03-26 11:41:51 +0800

    delete test cases

--- test/git.test.js
+++ test/git.test.js
@@ -3,21 +3,3 @@ const GitCommand = require("../models/git-command");
 
 const chai = require('chai');
 const expect = chai.expect;
-
-
-describe("Testing GitCommand.status()", function(){
-
-    it('Should return information if has changes in directory', function(){
-        let wd = new WorkingDirectory();
-        wd.addFile("index.html", "views", "<html>Hello</html>");
-        wd.addFile("index.js", "assets/scripts", "alert('Hi!')");
-
-        let git = new GitCommand(wd);
-        git.init();
-        git.add('views/index.html');
-        git.add('assets/scripts/index.js');
-        let output = git.status();
-
-        expect(output).to.equal('You have 2 change/s.\nviews/index.html\nassets/scripts/index.js');
-    });
-})
